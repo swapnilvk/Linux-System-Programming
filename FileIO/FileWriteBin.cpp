d8d5d5e7b25ff2d45616f0f469c92ddfc9e81029
swapnilvk <kulkarni.swapnil.v@gmal.com>
2021-04-07 10:24:33 +0530

    Read and Write binary file

--- FileIO/FileWriteBin.cpp
+++ FileIO/FileWriteBin.cpp
@@ -7,7 +7,7 @@ struct StThreeNumber
     int num1, num2, num3;
 };
 
-int main()
+void WriteTofile()
 {
     struct StThreeNumber number;
     int num;
@@ -19,7 +19,7 @@ int main()
         exit(1);
     }
 
-    for(int i=0; i<5; i++)
+    for(int i=1; i<5; i++)
     {
         number.num1 = i;
         number.num2 = 2*i;
@@ -27,5 +27,30 @@ int main()
 
         fwrite(&i, sizeof(struct StThreeNumber),1, fptr);
     }
+}
+void ReadFromFile()
+{
+    struct StThreeNumber number;
+    int num;
+    FILE *fptr = fopen("Program.bin", "rb");
+
+    if(fptr == NULL)
+    {
+        printf("\nERROR");
+        exit(1);
+    }
+
+    for(int i=1; i<5; i++)
+    {
+        fread  (&i, sizeof(struct StThreeNumber),1, fptr);
+        printf("num1: %d, num2: %d, num3: %d", number.num1, number.num2, number.num3);
+    }
+}
+
+int main()
+{
+    WriteTofile();
+    ReadFromFile();
+
     return 0;
 }
